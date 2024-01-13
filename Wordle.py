35f18b81c46c59c0d2e92d35809136033d38fb69
peterpan1905 <mrpiercewalker@gmail.com>
2024-01-12 22:23:55 -0700

    print key_word in top row

--- Wordle.py
+++ Wordle.py
@@ -11,13 +11,26 @@ from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
 def wordle():
-    key_word = random.choice(FIVE_LETTER_WORDS)
-    print(key_word)
+    # Choose random word from FIVE_LETTER_WORDS list as the solution
+    key_word = random.choice(FIVE_LETTER_WORDS).upper()
+
     def enter_action(s):
         gw.show_message("You have to implement this method.")
 
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
+
+    # Set row and column starting index values to 0
+    row = 0
+    col = 0
+
+    # Loop through each letter in the key_word and put it in it's proper row and column
+    for ch_position in range(len(key_word)):
+        ch = key_word[ch_position]
+
+        gw.set_square_letter(row, col, ch)
+
+        col += 1
 
 # Startup code
 
