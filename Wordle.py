1ef3631db05d11a01f73ae34413cb4adc0d108b5
Pierce Walker <133458978+peterpan1905@users.noreply.github.com>
2024-01-27 17:06:13 -0700

    Keyboard functionality and keyword selection (#4)

--- Wordle.py
+++ Wordle.py
@@ -11,11 +11,28 @@ from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
 def wordle():
+    # Initialize the keyword
+    keyword = ""
+    random_number = random.randint(0, len(FIVE_LETTER_WORDS))
+    keyword = FIVE_LETTER_WORDS[random_number]
 
     def enter_action(s):
         # Check if the word is in the list of valid English words
         if s.lower() in FIVE_LETTER_WORDS:
-            gw.show_message("I got this milestone working, woo!")
+            # Check if it is the keyword
+            if s.lower() == keyword:
+                # Handle the case when the correct keyword is entered
+                gw.show_message("You win!")
+            else:
+                # It's not the keyword, check if there is another row to advance to
+                current_row = gw.get_current_row()
+                if current_row < N_ROWS - 1:
+                    # Advance to the next row
+                    gw.set_current_row(current_row + 1)
+                    gw.show_message(f"Answer: {keyword}")
+                else:
+                    # You lost
+                    gw.show_message("Better luck next time!")
         else:
             gw.show_message("Not in word list")
 
